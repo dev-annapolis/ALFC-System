1618c37be6af84a93f9c814fdafb2cd9403df59f
Christopher Panerio <christopherpaneriotup@gmail.com>
2024-10-29 15:51:41 +0800

    Updates

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -2,12 +2,16 @@
 @section('content')
 
 
+<div class="container-fluid" style="height: 100vh; background-color: var(--primary-color-red)">
 
 
 
 
 
 
+
+
+</div>
 
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,11 @@
     <title>ALFC Insurance Agency Corporation</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-
     {{-- Bootstrap 5.3.3 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+
+    @vite('resources/js/app.js')
 
 </head>
 <body>
