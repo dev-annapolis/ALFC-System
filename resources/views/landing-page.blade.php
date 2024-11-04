541ba2e4c65cfa339a4a102485cf50bcf2db1401
Christopher Panerio <christopherpaneriotup@gmail.com>
2024-11-04 12:05:23 +0800

    updates

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,15 +1,144 @@
 @extends('layouts.app')
 @section('content')
 
+<style>
+    h1, h4 {
+        font-family: 'Times New Roman';
+        color: var(--lightest-grey);
+    }
 
-<div class="container-fluid" style="height: 100vh; background-color: var(--primary-color-red)">
+    h5 {
+        font-family: 'Times-New-Roman-Italic';
+    }
 
 
+    body{
+        background-color: var(--primary-color-red-bg)
+    }
+
+     /* Small (sm): ≥576px */
+     @media (max-width: 767.98px) {
 
 
+        .alfc-sub-title{
+            font-size: 30px;;
+        }
+
+        .alfc-logo{
+            height: 100px;
+            width: 95px;
+        }
+
+        .motto{
+            margin-top: 10px;
+        }
+
+    }
+
+    /* Medium (md): ≥768px */
+    @media (min-width: 768px) and (max-width: 991.98px) {
+        .alfc-title{
+            font-size: 50px;;
+        }
+
+        .alfc-sub-title{
+            font-size: 35px;;
+        }
+
+        .alfc-logo{
+            height: 100px;
+        }
+
+        .alfc-logo{
+            height: 130px;
+            width: 130px;
+        }
+    }
+
+    /* Large (lg): ≥992px */
+    @media (min-width: 992px) and (max-width: 1199.98px) {
+        .alfc-title{
+            font-size: 60px;;
+        }
+
+        .alfc-sub-title{
+            font-size: 40px;;
+        }
+
+        .alfc-logo{
+            height: 150px;
+            width: 200px;
+        }
+    }
+
+    /* Extra large (xl): ≥1200px */
+    @media (min-width: 1200px) and (max-width: 1399.98px) {
+
+        .alfc-title{
+            font-size: 70px;;
+        }
+
+        .alfc-sub-title{
+            font-size: 40px;;
+        }
+        /* Styles for extra large screens go here */
+        .alfc-logo{
+            height: 200px;
+            width: 200px;
+        }
+    }
+
+    /* Extra extra large (xxl): ≥1400px */
+    @media (min-width: 1400px) {
+
+        .alfc-title{
+            font-size: 80px;;
+        }
+
+        .alfc-sub-title{
+            font-size: 40px;;
+        }
+        /* Styles for extra extra large screens go here */
+        .alfc-logo{
+            height: 180px;
+            width: 180px;
+        }
+    }
+
+</style>
+
+<div class="container-fluid d-flex justify-content-center align-items-center" style="min-height: 100vh; min-width: 100vw;">
+
+    <div class="row justify-content-center">
+
+        <div class="col-9 col-sm-9 mb-0 mt-md-5">
+
+            <div class="d-flex flex-column flex-md-row align-items-center">
+
+                <div class="col-md-4 col-sm-6  d-flex justify-content-center p-4">
+                    <img class="alfc-logo img-fluid" src="{{ asset('images/frontend/alfc-logo.jpg') }}" alt="ALFC Logo">
+                </div>
 
 
+                <div class="col-md-10 col-lg-9 col-sm-12 d-flex flex-column">
+                    <h1 class="alfc-title display-2 text-center text-md-start mb-0 fw-bold">ALFC</h1>
 
+                    <h1 class="alfc-sub-title display-4  text-center text-md-start mt-0">Insurance Agency Corporation</h1>
+
+                    <h5 class="motto fs-3 text-center text-lg-end text-md-start text-white ">Geared to secure</h5>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-md-7 col-sm-10 mt-5 mb-5 d-flex justify-content-center">
+            <a href="#" class="btn btn-light mt-3 fw-bold bg-white fs-5 w-50 w-md-25" style="color: var(--primary-color-red)">
+                 Get Started
+            </a>
+        </div>
+
+    </div>
 
 </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,26 @@
 </head>
 <body>
 
+    <div id="viewport-info" style="
+    position: fixed;
+    top: 90%;
+    left: 50%;
+    z-index: 10000;
+    transform: translate(-50%, -50%);
+    background: rgba(247, 201, 241, 0.4);
+    padding: .5rem 1rem;
+    border-radius: 30px;
+">
+    <div id="xs" class="d-block d-sm-none"></div>
+    <div id="sm" class="d-none d-sm-block d-md-none"></div>
+    <div id="md" class="d-none d-md-block d-lg-none"></div>
+    <div id="lg" class="d-none d-lg-block d-xl-none"></div>
+    <div id="xl" class="d-none d-xl-block d-xxl-none"></div>
+    <div id="xxl" class="d-none d-xxl-block"></div>
+</div>
+
+
+
     @yield('content')
 
 
@@ -27,6 +47,22 @@
     {{-- Bootstrap 5.3.3 --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
+
+
+    <script>
+        function updateViewportDimensions() {
+            const width = window.innerWidth;
+            document.getElementById("xs").textContent = width < 576 ? `Extra Small (<576px): ${width}px` : '';
+            document.getElementById("sm").textContent = width >= 576 && width < 768 ? `Small (≥576px): ${width}px` : '';
+            document.getElementById("md").textContent = width >= 768 && width < 992 ? `Medium (≥768px): ${width}px` : '';
+            document.getElementById("lg").textContent = width >= 992 && width < 1200 ? `Large (≥992px): ${width}px` : '';
+            document.getElementById("xl").textContent = width >= 1200 && width < 1400 ? `X-Large (≥1200px): ${width}px` : '';
+            document.getElementById("xxl").textContent = width >= 1400 ? `XX-Large (≥1400px): ${width}px` : '';
+        }
+
+        window.addEventListener('resize', updateViewportDimensions);
+        window.addEventListener('load', updateViewportDimensions);
+    </script>
 
 </body>
 </html>
