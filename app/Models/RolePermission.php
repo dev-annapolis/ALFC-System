6809087627a9b171b542f62e712b1fb63ee4d470
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-05 15:54:24 +0800

    reole P model

--- app/Models/RolePermission.php
+++ app/Models/RolePermission.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class RolePermission extends Model
 {
-    //
+    protected $fillable = ['role_id', 'table_name', 'column_name', 'can_view', 'can_edit'];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
