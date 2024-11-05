44c62f4bec809019f43c0193ca73d13212b292e9
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-05 15:55:15 +0800

    role P mig

---
+++ database/migrations/2024_11_04_083637_create_role_permissions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->string('table_name');
+            $table->string('column_name'); 
+            $table->boolean('can_view')->default(false);
+            $table->boolean('can_edit')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role_permissions');
+    }
+};
