f2f53caccfe744acdc5896b6a32008e44c5e2020
John Cris Fernando <fernandojohncris@gmail.com>
2024-11-05 13:56:35 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('landing-page');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/form', [App\Http\Controllers\HomeController::class, 'showForm'])->name('form.show');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/form', [HomeController::class, 'showForm'])->name('form.show');
