dabff006dc3e3486fafcd8f092a5583886aa591f
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-05 15:54:16 +0800

    role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $fillable = ['name', 'view_name'];
+
+    public function permissions()
+    {
+        return $this->hasMany(RolePermission::class);
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
