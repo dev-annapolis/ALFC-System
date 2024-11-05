d034d6913c5d3e02b96b060d9436156a2791f8b3
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-05 15:56:15 +0800

    web route

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,15 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/form', [HomeController::class, 'showForm'])->name('form.show');
+
+
+
+
+
+
+use App\Http\Controllers\UniversalTableController;
+Route::get('/universal-table', [UniversalTableController::class, 'showRecords'])->middleware('auth');
+
+use App\Http\Controllers\RolePermissionController;
+Route::get('/permissions/edit', [RolePermissionController::class, 'edit'])->name('permissions.edit');
+Route::post('/permissions/update', [RolePermissionController::class, 'update'])->name('permissions.update');
