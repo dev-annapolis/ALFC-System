556014eaf2f2fa35699c68f565ff9306c5ced9a7
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-06 09:39:45 +0800

    sources mig

--- database/migrations/2024_11_04_082704_create_sources_table.php
+++ database/migrations/2024_11_04_082704_create_sources_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('sources', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
