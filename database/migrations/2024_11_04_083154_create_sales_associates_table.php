e525c9d7fc28ef6fb2dd5d6b855535fde51a0647
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-06 09:41:47 +0800

    sa sm mig

--- database/migrations/2024_11_04_083154_create_sales_associates_table.php
+++ database/migrations/2024_11_04_083154_create_sales_associates_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('sales_associates', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('team_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
