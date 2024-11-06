4a5e4924954d793d8c3494c697b2e03785263446
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-06 11:46:37 +0800

    sales ass  model

--- app/Models/SalesAssociate.php
+++ app/Models/SalesAssociate.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class SalesAssociate extends Model
 {
-    //
+    protected $fillable = ['user_id', 'team_id', 'name', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function insuranceDetails()
+    {
+        return $this->hasMany(InsuranceDetail::class);
+    }
+
 }
