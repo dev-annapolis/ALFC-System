099a0fb88488391d5a3f7046848fbfe3a00cef17
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-06 11:44:51 +0800

    collection d  model

--- app/Models/CollectionDetail.php
+++ app/Models/CollectionDetail.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class CollectionDetail extends Model
 {
-    //
+    protected $fillable = [
+        'insurance_detail_id', 'insurance_type', 'sale_status', 'tele_id', 'due_date', 
+        'paid_terms', 'payment_remarks', 'account_status', 'payment_ptp_declared', 
+        'payment_center', 'reference_number', 'date_on_receipt_abstract', 
+        'contact_number_verification'
+    ];
+    
+    public function insuranceDetail()
+    {
+        return $this->belongsTo(InsuranceDetail::class);
+    }
+    
+    public function tele()
+    {
+        return $this->belongsTo(Tele::class);
+    }
+    
 }
