c2b8ebaf236e15f95cf3d11771d724fdb09f0c49
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-06 09:42:32 +0800

    commisision d mig

--- database/migrations/2024_11_04_083541_create_commision_details_table.php
+++ database/migrations/2024_11_04_083541_create_commision_details_table.php
@@ -13,6 +13,25 @@ return new class extends Migration
     {
         Schema::create('commision_details', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('insurance_detail_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('provision_receipt')->nullable();
+
+            $table->string('gross_premium')->nullable();
+            $table->string('discount')->nullable();
+            $table->string('net_discounted')->nullable();
+            $table->string('amount_due_to_provider')->nullable();
+            $table->string('full_commission')->nullable();
+
+            $table->string('marketing_fund')->nullable();
+            $table->string('offsetting')->nullable();
+            $table->string('promo')->nullable();
+            $table->string('total_commission')->nullable();
+
+            $table->string('vat')->nullable();
+            $table->string('sales_credit')->nullable();
+            $table->string('sales_credit_percent')->nullable();
+            $table->string('comm_deduct')->nullable();
+
             $table->timestamps();
         });
     }
