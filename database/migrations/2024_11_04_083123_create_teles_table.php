cc60ff2323a2f403da688115a1b2cc64a8c6ddd1
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-06 09:41:38 +0800

    teles mig

--- database/migrations/2024_11_04_083123_create_teles_table.php
+++ database/migrations/2024_11_04_083123_create_teles_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('teles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
