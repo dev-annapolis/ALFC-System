fc0ba2831b6b288530148beb5f51753f69544093
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-06 09:42:16 +0800

    insurance d mig

--- database/migrations/2024_11_04_083528_create_insurance_details_table.php
+++ database/migrations/2024_11_04_083528_create_insurance_details_table.php
@@ -13,6 +13,51 @@ return new class extends Migration
     {
         Schema::create('insurance_details', function (Blueprint $table) {
             $table->id();
+            $table->string('issuance_code');
+            $table->foreignId('assured_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('sales_associate_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('sale_date')->nullable();
+            $table->string('classification');
+
+            $table->string('insurance_type')->nullable();
+            $table->string('sale_status')->nullable();
+
+            $table->foreignId('branch_manager_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('legal_representative_name')->nullable();
+            $table->string('legal_supervisor_name')->nullable();
+            $table->string('assigned_atty_one')->nullable();
+            $table->string('assigned_atty_two')->nullable();
+            $table->string('collection_gm')->nullable();
+
+            $table->foreignId('product_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('subproduct_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('product_type')->nullable();
+            $table->foreignId('source_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('source_branch_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('if_gdfi_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('mortgagee')->nullable();
+            $table->foreignId('area_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('alfc_branch_id')->nullable()->constrained()->onDelete('cascade');
+
+            $table->string('loan_amount')->nullable();
+            $table->string('total_sum_insured')->nullable();
+
+            $table->string('policy_number')->nullable();
+            $table->string('policy_insumption_date')->nullable();
+            $table->string('expiry_date')->nullable();
+            $table->string('plate_conduction_number')->nullable();
+            $table->string('description')->nullable();
+
+            $table->string('policy_expiration_aging')->nullable();
+            $table->string('book_number')->nullable();
+            $table->string('filing_number')->nullable();
+            $table->string('pid_received_date')->nullable();
+            $table->string('pid_completion_date')->nullable();
+            $table->string('remarks')->nullable();
+
+            $table->string('mode_of_payment_id')->nullable();
+            $table->string('provider_id')->nullable();
+
             $table->timestamps();
         });
     }
