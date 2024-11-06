db68beae502b51f4182f843cdab3c77f8af81a94
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-06 11:44:30 +0800

    assured  model

--- app/Models/Assured.php
+++ app/Models/Assured.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Assured extends Model
 {
-    //
+    protected $fillable = ['name', 'assured_detail_id'];
+
+    public function assuredDetail()
+    {
+        return $this->belongsTo(AssuredDetail::class);
+    }
+
+    public function insuranceDetails()
+    {
+        return $this->hasMany(InsuranceDetail::class);
+    }
+
 }
