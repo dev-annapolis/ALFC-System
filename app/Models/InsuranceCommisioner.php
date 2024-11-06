ac7a3520b3f6ea881312583598918766a5fda743
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-06 11:45:25 +0800

    insurance commisioner  model

--- app/Models/InsuranceCommisioner.php
+++ app/Models/InsuranceCommisioner.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class InsuranceCommisioner extends Model
 {
-    //
+    protected $fillable = ['insurance_detail_id', 'commisioner_id'];
+
+    public function insuranceDetail()
+    {
+        return $this->belongsTo(InsuranceDetail::class);
+    }
+
+    public function commisioner()
+    {
+        return $this->belongsTo(Commisioner::class);
+    }
+
 }
