f009aa7d2c407ba6d660a11e046e274bdf0c00cc
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-18 11:15:44 +0800

    fix model

--- app/Models/InsuranceCommisioner.php
+++ app/Models/InsuranceCommisioner.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class InsuranceCommisioner extends Model
 {
-    protected $fillable = ['insurance_detail_id', 'commisioner_id'];
+    protected $fillable = [
+        'insurance_detail_id', 
+        'commisioner_id', 
+        'commisioner_name', 
+        'amount'
+    ];
 
     public function insuranceDetail()
     {
