fa8d1f2500f0f75eec5f417c740bffaf7924c963
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-18 11:15:51 +0800

    fix model

--- app/Models/PaymentDetail.php
+++ app/Models/PaymentDetail.php
@@ -7,12 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentDetail extends Model
 {
     protected $fillable = [
-        'insurance_detail_id', 'payment_terms', 'due_date', 'schedule_first_payment',
-        'schedule_second_payment', 'schedule_third_payment', 'schedule_fourth_payment', 
-        'schedule_fifth_payment', 'schedule_sixth_payment', 'schedule_seventh_payment', 
-        'schedule_eight_payment', 'for_billing', 'over_under_payment', 'initial_payment', 
-        'good_as_sales_date', 'status', 'ra_comments', 'admin_assistant_remarks', 
-        'tracking_number', 'policy_received_by'
+        'insurance_detail_id', 
+        'payment_terms', 
+        'due_date_start', 
+        'due_date_end', 
+        'first_payment_schedule', 
+        'first_payment_amount', 
+        'second_payment_schedule', 
+        'second_payment_amount', 
+        'third_payment_schedule', 
+        'third_payment_amount', 
+        'fourth_payment_schedule', 
+        'fourth_payment_amount', 
+        'fifth_payment_schedule', 
+        'fifth_payment_amount', 
+        'sixth_payment_schedule', 
+        'sixth_payment_amount', 
+        'seventh_payment_schedule', 
+        'seventh_payment_amount', 
+        'eight_payment_schedule', 
+        'eight_payment_amount', 
+        'provision_receipt', 
+        'initial_payment', 
+        'for_billing', 
+        'over_under_payment', 
+        'date_of_good_as_sales', 
+        'payment_status'
     ];
     
     public function insuranceDetail()
