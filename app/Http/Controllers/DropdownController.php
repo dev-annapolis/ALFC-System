9ada603a5ecc37e827d9626b78cae2ba7b65d2e4
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-23 13:38:03 +0800

    dropdown controller

--- app/Http/Controllers/DropdownController.php
+++ app/Http/Controllers/DropdownController.php
@@ -4,7 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Team;
+
 class DropdownController extends Controller
 {
-    //
+    public function teamsIndex()
+    {
+        $teams = Team::all();
+        return view('dropdown.teams', compact( 'teams'));
+    }
+
+    public function teamsAdd()
+    {
+
+    }
+
+    public function teamsChangeStatus($id)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
