84683f5aa8c22d4c47862c7ef3e16836d2e46374
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-23 13:37:53 +0800

    teams routes

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,12 @@ Route::get('/api/sales-report', [SalesReportController::class, 'salesReportData'
 // Route::get('/api/insurance-detail/{id}', [SalesReportController::class, 'fetchInsuranceDetail']);
 Route::get('/api/insurance/details/{id}', [SalesReportController::class, 'showInsuranceDetails']);
 Route::post('/api/insurance/details/update', [SalesReportController::class, 'updateInsuranceDetail']);
+
+
+
+
+
+use App\Http\Controllers\DropdownController;
+Route::get('/teamsIndex', [DropdownController::class, 'teamsIndex'])->name('teams.index');
+Route::post('/teams/store', [DropdownController::class, 'store'])->name('teams.store');
+Route::put('/teams/update', [DropdownController::class, 'update'])->name('teams.update');
