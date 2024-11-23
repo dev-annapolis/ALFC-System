9ec8c327962ed33c12c41d63c6076c3f952c15ad
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-23 15:50:02 +0800

    auth

--- app/Http/Controllers/DropdownController.php
+++ app/Http/Controllers/DropdownController.php
@@ -3,19 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\Team;
 
 class DropdownController extends Controller
 {
+    private function checkRole()
+    {
+        if (Auth::check() && in_array(Auth::user()->role_id, [1, 2])) {
+            return true;
+        } else {
+            return abort(403, 'Unauthorized access.');
+        }
+    }
+
     public function teamsIndex()
     {
+        $this->checkRole();
         $teams = Team::orderBy('status', 'asc')->get();
         return view('dropdown.teams', compact( 'teams'));
     }
 
     public function teamsStore(Request $request)
     {
+        $this->checkRole();
+
         $validated = $request->validate([
             'name' => 'required|string|max:255|unique:teams,name',
         ]);
@@ -29,6 +42,7 @@ class DropdownController extends Controller
 
     public function teamsUpdate(Request $request)
     {
+        $this->checkRole();
         $validated = $request->validate([
             'id' => 'required|exists:teams,id',
             'name' => 'required|string|max:255|unique:teams,name,' . $request->id,
@@ -44,6 +58,7 @@ class DropdownController extends Controller
 
     public function teamsChangeStatus($id)
     {
+        $this->checkRole();
         $team = Team::findOrFail($id);
         $team->status = $team->status === 'active' ? 'inactive' : 'active';
         $team->save();
