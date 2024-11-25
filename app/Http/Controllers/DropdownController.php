e225e95535beee75fe1e12e86b521761a918651b
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-11-25 09:56:12 +0800

    controler remove checkrole

--- app/Http/Controllers/DropdownController.php
+++ app/Http/Controllers/DropdownController.php
@@ -9,26 +9,15 @@ use App\Models\Team;
 
 class DropdownController extends Controller
 {
-    private function checkRole()
-    {
-        if (Auth::check() && in_array(Auth::user()->role_id, [1, 2])) {
-            return true;
-        } else {
-            return abort(403, 'Unauthorized access.');
-        }
-    }
 
     public function teamsIndex()
     {
-        $this->checkRole();
         $teams = Team::orderBy('status', 'asc')->get();
         return view('dropdown.teams', compact( 'teams'));
     }
 
     public function teamsStore(Request $request)
     {
-        $this->checkRole();
-
         $validated = $request->validate([
             'name' => 'required|string|max:255|unique:teams,name',
         ]);
@@ -42,7 +31,6 @@ class DropdownController extends Controller
 
     public function teamsUpdate(Request $request)
     {
-        $this->checkRole();
         $validated = $request->validate([
             'id' => 'required|exists:teams,id',
             'name' => 'required|string|max:255|unique:teams,name,' . $request->id,
@@ -58,7 +46,6 @@ class DropdownController extends Controller
 
     public function teamsChangeStatus($id)
     {
-        $this->checkRole();
         $team = Team::findOrFail($id);
         $team->status = $team->status === 'active' ? 'inactive' : 'active';
         $team->save();
