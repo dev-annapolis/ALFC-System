f281195c17e1c5f68c9344ff4e6b1c67a0aeed1f
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2024-12-12 13:25:56 +0800

    chklst ttle migrations with variables

--- database/migrations/2024_12_12_051143_create_checklist_titles_table.php
+++ database/migrations/2024_12_12_051143_create_checklist_titles_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('checklist_titles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
