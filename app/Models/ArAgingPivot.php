ae10b979d4f4655a72367f7e5a757c77b4c9f551
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2025-01-03 09:25:20 +0800

    ar aging pivot

--- app/Models/ArAgingPivot.php
+++ app/Models/ArAgingPivot.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArAgingPivot extends Model
 {
-    //
+    protected $fillable = [
+        'ar_aging_id',
+        'label',
+        'payment_amount',
+        'payment_schedule',
+        'paid_amount',
+        'paid_schedule',
+        'reference_number',
+        'ra_remarks',
+        'tele_remarks',
+        'paid'
+    ];
+    
+    public function arAging()
+    {
+        return $this->belongsTo(ArAging::class);
+    }
 }
