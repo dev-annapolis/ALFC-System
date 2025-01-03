4e8a1a1d4b1db1112c8e669b93cb730e23a9fc27
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2025-01-03 09:25:41 +0800

    ar aging migration

--- database/migrations/2025_01_02_072327_create_ar_agings_table.php
+++ database/migrations/2025_01_02_072327_create_ar_agings_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('ar_agings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('insurance_detail_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('issuance_code')->nullable();
+            $table->string('name')->nullable();
+            $table->string('due_date')->nullable();
+            $table->string('terms')->nullable();
+            $table->string('team')->nullable();
+            $table->string('policy_number')->nullable();
+            $table->string('sale_date')->nullable();
+            $table->string('mode_of_payment')->nullable();
+            $table->string('gross_premium')->nullable();
+            $table->string('total_outstanding')->nullable();
+            $table->string('balance')->nullable();
             $table->timestamps();
         });
     }
