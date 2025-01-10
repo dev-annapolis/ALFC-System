954e7d77ce7c27fdf8909c01f8b68b78d3a9007a
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2025-01-10 15:00:32 +0800

    source div model

--- app/Models/SourceDivision.php
+++ app/Models/SourceDivision.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SourceDivision extends Model
 {
-    //
+    protected $fillable = ['source_id', 'name', 'status'];
+
+    public function source()
+    {
+        return $this->belongsTo(Source::class);
+    }
+    
+    public function insuranceDetails()
+    {
+        return $this->hasMany(InsuranceDetail::class);
+    }
 }
