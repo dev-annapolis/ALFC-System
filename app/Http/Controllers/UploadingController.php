4dc596c7e6e5bdb47ce73179e21c5e79b2139484
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2025-01-13 13:35:53 +0800

    upload index

--- app/Http/Controllers/UploadingController.php
+++ app/Http/Controllers/UploadingController.php
@@ -12,4 +12,26 @@ class UploadingController extends Controller
 
         return view('uploading.uploadDataIndex', compact( 'userID'));
     }
+
+    public function uploadDataStore()
+    {
+        $userID = auth()->user()->id;
+
+        $validator = Validator::make($request->all(), [
+            'excel_file' => 'required|file|mimes:xls,xlsx',
+        ]);
+
+        if ($validator->fails()) {
+            Log::info("PLEASE INPUT VALID EXCEL FILE");
+            return back()->with('error', "PLEASE INPUT VALID EXCEL FILE");
+        }
+
+        $file = $request->file('excel_file');
+        $import = new DataImport();
+        Excel::import($import, $file);
+
+        usleep(500000);
+        Log::info('Excel data has been successfully imported with no errors');
+        return back()->with(['uploadSuccess' => true]);
+    }
 }
