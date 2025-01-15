ed810fd7b505c378460656fdd4a2cd487c27de74
James Christian Villamin <132872390+dyimz@users.noreply.github.com>
2025-01-15 15:43:51 +0800

    rm model

--- app/Models/RegionalManager.php
+++ app/Models/RegionalManager.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegionalManager extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 
+        'team_id', 
+        'name', 
+        'status'
+    ];
+
+    public function insuranceDetails()
+    {
+        return $this->hasMany(InsuranceDetail::class);
+    }
 }
